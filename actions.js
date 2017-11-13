382ee4aed3259ad8de4d1bd32b4d0d8419ada5e3
Kasia Fernandes <fernandes.kasia@gmail.com>
2017-11-13 23:20:52 +0100

    second commit

--- actions.js
+++ actions.js
@@ -1,3 +1,5 @@
+import uuid from uuid;
+
 const ADD_COMMENT = 'ADD_COMMENT';
 const EDIT_COMMENT = 'EDIT_COMMENT';
 const REMOVE_COMMENT = 'REMOVE_COMMENT';
@@ -12,39 +14,38 @@ function addComment(text) {
     }
 }
 
-function editComment(text) {
+function editComment(text, id) {
     return {
         type: EDIT_COMMENT,
         text,
-        id: uuid.v4()
+        id
     }
 }
 
-function removeComment() {
+function removeComment(id) {
     return {
         type: REMOVE_COMMENT,
-        id: uuid.v4()
+        id
     }
 }
 
-function thumbUpComment(thumbs) {
+function thumbUpComment(thumbs, id) {
     return {
         type: THUMB_UP_COMMENT,
-        id: uuid.v4(),
+        id,
         thumbs
     }
 }
 
-function thumbDownComment(thumbs) {
+function thumbDownComment(id) {
     return {
         type: THUMB_DOWN_COMMENT,
-        id: uuid.v4(),
-        thumbs
+        id,
     }
 }
 
 const boundAddComment = text => dispatch(addComment(text));
-const boundEditComment = text => dispatch(editComment(text));
-const boundRemoveComment = () => dispatch(removeComment());
-const boundThumbUpComment = thumbs => dispatch(thumbUpComment(thumbs));
-const boundThumbDownComment = thumbs => dispatch(thumbDownComment(thumbs));
+const boundEditComment = (text, id) => dispatch(editComment(text, id));
+const boundRemoveComment = id => dispatch(removeComment(id));
+const boundThumbUpComment = id => dispatch(thumbUpComment(id));
+const boundThumbDownComment = id => dispatch(thumbDownComment(id));
